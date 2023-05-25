5b7c31f001c554bc4b37bba5c7655360d35431e1
Zainab460 <zainabdavids460@gmail.com>
2023-05-25 18:16:25 +0200

    initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Form from "./components/Form/Form";
+import Notes from "./components/Notes/Notes";
+import Modal from "./components/Modal/Modal";
+// like html elements
+const NOTES = [];
+const App = () => {
+  const [notes, setNotes] = useState(NOTES);
+  const [selectedNote, setSelectedNote] = useState({});
+  const [isOpenModal, setOpenModal] = useState(false);
 
-function App() {
+  const addNote = (note) => {
+    setNotes((prevNotes) => {
+      return [...notes, note];
+    });
+  };
+  const editNote = (editedNote) => {
+    setNotes((prevNotes) => {
+      const newNote = prevNotes.map (note => {
+        console.log("before", note)
+        if (editedNote.id === note.id) {
+          note.title = editedNote.title;
+          note.text = editedNote.text;
+        } 
+        console.log("After", note)
+        return note
+      })
+      return newNote
+    });
+  }
+  const deleteNote = (id) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => id !== note.id);
+    });
+  };
+  const handleModal = () => {
+    setOpenModal((prevNotes) => {
+      // return !prevNotes
+      if (prevNotes) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Sidebar />
+      <Form addNote={addNote} />
+      <Notes
+        notes={notes}
+        deleteNote={deleteNote}
+        handleModal={handleModal}
+        setSelectedNote={setSelectedNote}
+      />
+      {isOpenModal && (<Modal isOpenModal={isOpenModal} selectedNote ={selectedNote} handleModal={handleModal} editNote ={editNote}/>)}
+      
     </div>
   );
-}
+};
 
 export default App;
